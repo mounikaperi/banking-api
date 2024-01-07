ff58ff4d24246d9286d357cfdf0d11fb2e6f4bd9
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-01-07 18:52:53 +0530

    Implemented EasyMoneyUsernamepasswordAuthenticationProvider

--- src/main/java/com/springbanking/easymoney/controller/LoginController.java
+++ src/main/java/com/springbanking/easymoney/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.springbanking.easymoney.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
     @Autowired
     CustomerRepository customerRepository;
-
+    @Autowired
+    PasswordEncoder passwordEncoder;
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
         Customer savedCustomer = null;
         ResponseEntity response = null;
         try {
+            String hashedPassword = passwordEncoder.encode(customer.getPassword());
+            customer.setPassword(hashedPassword);
             savedCustomer = customerRepository.save(customer);
             if (savedCustomer.getId() > 0) {
                 response = ResponseEntity
