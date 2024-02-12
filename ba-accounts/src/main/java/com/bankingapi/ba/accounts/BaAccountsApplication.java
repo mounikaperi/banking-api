39555ad8491602f50472818c2f4ce225051d8ee7
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-02-12 15:18:52 +0530

    Initialized banking application accounts repository

---
+++ ba-accounts/src/main/java/com/bankingapi/ba/accounts/BaAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.bankingapi.ba.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaAccountsApplication.class, args);
+	}
+
+}
---
+++ ba-accounts/src/test/java/com/bankingapi/ba/accounts/BaAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingapi.ba.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
