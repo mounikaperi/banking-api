632a3bd351de440ef9e64ab417a11c99ce13bbdd
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-02-18 16:16:11 +0530

    Initial commit on cards repository

---
+++ ba-cards/src/main/java/com/bankingapi/bacards/BaCardsApplication.java
@@ -0,0 +1,13 @@
+package com.bankingapi.bacards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaCardsApplication.class, args);
+	}
+
+}
