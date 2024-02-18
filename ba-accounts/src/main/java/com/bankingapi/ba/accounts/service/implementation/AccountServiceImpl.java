db6e18eb46614c7c1c0ed5a5b67be86d5c48007f
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-02-18 15:47:43 +0530

    Fetch Account Details of a customer based on mobile number

--- ba-accounts/src/main/java/com/bankingapi/ba/accounts/service/implementation/AccountServiceImpl.java
+++ ba-accounts/src/main/java/com/bankingapi/ba/accounts/service/implementation/AccountServiceImpl.java
@@ -1,7 +1,13 @@
 package com.bankingapi.ba.accounts.service.implementation;
 
+import com.bankingapi.ba.accounts.constants.AccountsConstants;
+import com.bankingapi.ba.accounts.dto.AccountsDTO;
 import com.bankingapi.ba.accounts.dto.CustomerDTO;
+import com.bankingapi.ba.accounts.entities.Accounts;
 import com.bankingapi.ba.accounts.entities.Customer;
+import com.bankingapi.ba.accounts.exception.CustomerAlreadyExistsException;
+import com.bankingapi.ba.accounts.exception.ResourceNotFoundException;
+import com.bankingapi.ba.accounts.mapper.AccountsMapper;
 import com.bankingapi.ba.accounts.mapper.CustomerMapper;
 import com.bankingapi.ba.accounts.repository.AccountsRepository;
 import com.bankingapi.ba.accounts.repository.CustomerRepository;
@@ -10,6 +16,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -27,5 +34,27 @@ public class AccountServiceImpl implements IAccountsService {
         }
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
+    }
+
+    private Accounts createNewAccount(Customer customer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccountNumber = 1000000000L + new Random().nextInt(900000000);
+        newAccount.setAccountNumber(randomAccountNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        return newAccount;
+    }
+
+    public CustomerDTO fetchAccount(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+        CustomerDTO customerDTO = CustomerMapper.mapToCustomerDTO(customer, new CustomerDTO());
+        customerDTO.setAccountsDTO(AccountsMapper.mapToAccountsDTO(accounts, new AccountsDTO()));
+        return customerDTO;
     }
 }
