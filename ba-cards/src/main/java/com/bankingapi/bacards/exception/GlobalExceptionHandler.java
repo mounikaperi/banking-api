38ac68789c8507155bb2307299c3bec6c856845d
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-02-19 18:17:10 +0530

    Cards Exceptions

---
+++ ba-cards/src/main/java/com/bankingapi/bacards/exception/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.bankingapi.bacards.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
+ 
