42a40e1353b750393abe6aac79a4d119bb60aea4
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-02-21 13:11:22 +0530

    Implementation logic for cards service

--- ba-cards/src/main/java/com/bankingapi/bacards/service/implementation/CardsServiceImplementation.java
+++ ba-cards/src/main/java/com/bankingapi/bacards/service/implementation/CardsServiceImplementation.java
@@ -1,7 +1,11 @@
 package com.bankingapi.bacards.service.implementation;
 
+import com.bankingapi.bacards.cards.CardsConstants;
+import com.bankingapi.bacards.dto.CardsDTO;
 import com.bankingapi.bacards.entity.Cards;
 import com.bankingapi.bacards.exception.CardAlreadyExistsException;
+import com.bankingapi.bacards.exception.ResourceNotFoundException;
+import com.bankingapi.bacards.mapper.CardsMapper;
 import com.bankingapi.bacards.repository.CardsRepository;
 import com.bankingapi.bacards.service.ICardsService;
 import lombok.AllArgsConstructor;
@@ -30,10 +34,33 @@ public class CardsServiceImplementation implements ICardsService {
         newCard.setMobileNumber(mobileNumber);
         long generatedCardNumber = 1000000000000L + new Random().nextInt(900000000);
         newCard.setCardNumber(Long.toString(generatedCardNumber));
-        newCard.setCardType(CardsContants.CREDIT_CARD);
+        newCard.setCardType(CardsConstants.CREDIT_CARD);
         newCard.setTotalLimit(CardsConstants.NEW_CARD_LIMIT);
         newCard.setAmountUsed(0);
         newCard.setAvailableAmount(CardsConstants.NEW_CARD_LIMIT);
         return newCard;
+    }
+
+    public CardsDTO fetchCard(String mobileNumber) {
+        Cards cards = cardsRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Card", "mobileNumber", mobileNumber);
+        );
+        return CardsMapper.mapToCardsDTO(cards, new CardsDTO());
+    }
+
+    public boolean updateCard(CardsDTO cardsDTO) {
+        Cards cards = cardsRepository.findByCardNumber(cardsDTO.getCardNumber()).orElseThrow(
+                () -> new ResourceNotFoundException("Card", "CardNumber", cardsDTO.getCardNumber())
+        );
+        CardsMapper.mapToCards(cardsDTO, cards);
+        cardsRepository.save(cards);
+        return true;
+    }
+    public boolean deleteCard(String mobileNumber) {
+        Cards cards = cardsRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Card", "mobileNumber", mobileNumber)
+        );
+        cardsRepository.deleteById(cards.getCardId());
+        return true;
     }
 }
