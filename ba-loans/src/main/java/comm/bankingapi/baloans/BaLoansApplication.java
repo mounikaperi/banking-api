6a18d5589d649735d7ba7f284fd99e380512b1e9
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-02-22 11:19:10 +0530

    Started working on loans application

---
+++ ba-loans/src/main/java/comm/bankingapi/baloans/BaLoansApplication.java
@@ -0,0 +1,13 @@
+package comm.bankingapi.baloans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaLoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaLoansApplication.class, args);
+	}
+
+}
---
+++ ba-loans/src/test/java/comm/bankingapi/baloans/BaLoansApplicationTests.java
@@ -0,0 +1,13 @@
+package comm.bankingapi.baloans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaLoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
