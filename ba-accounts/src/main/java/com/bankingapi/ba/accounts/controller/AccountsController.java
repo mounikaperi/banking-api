eb66abb1fdc6cc6d222fa205b2d0575a97da2717
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-02-25 17:34:44 +0530

    Defining Accounts Service Controller

---
+++ ba-accounts/src/main/java/com/bankingapi/ba/accounts/controller/AccountsController.java
@@ -0,0 +1,71 @@
+package com.bankingapi.ba.accounts.controller;
+
+import com.bankingapi.ba.accounts.constants.AccountsConstants;
+import com.bankingapi.ba.accounts.dto.CustomerDTO;
+import com.bankingapi.ba.accounts.dto.ErrorResponseDTO;
+import com.bankingapi.ba.accounts.dto.ResponseDTO;
+import com.bankingapi.ba.accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "CRUD REST APIs for Accounts in EazyBank")
+@RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
+@Validated
+public class AccountsController {
+
+    private IAccountsService iAccountsService;
+
+    @Operation(summary = "Create Account REST API", description = "REST API to create new Customer")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "HTTP Status CREATED"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(name = ErrorResponseDTO.class)))
+    })
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO> createAccount(@Valid @RequestBody CustomerDTO customerDTO) {
+        iAccountsService.createAccount(customerDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDTO(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+    }
+
+    @Operation(summary = "Fetch Account Details REST API", description = "REST APII to fetch customer and Account details based on a mobile number")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    })
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDTO> fetchAccountDetails(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobilenumber must be 10 digits") String mobileNumber) {
+        CustomerDTO customerDTO = iAccountsService.fetchAccount(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(customerDTO);
+    }
+
+    @Operation(summary = "Update Account Details REST API", description = "REST API to update Customer and Account details on a account number")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP status OK"),
+            @ApiResponse(responseCode = "417", description = "Expectation Failed"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    })
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDTO> updateAccountDetails(@Valid @RequestBody CustomerDTO customerDTO) {
+        boolean isUpdated = iAccountsService.updateAccount(customerDTO);
+        if (isUpdated) {
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDTO(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDTO(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+    
+}
