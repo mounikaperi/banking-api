553c255cb8a55d7938b9908c43c796612f631f78
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-02-25 16:50:02 +0530

    Implementing Loans Controller

--- ba-loans/src/main/java/com/bankingapi/baloans/controller/LoansController.java
+++ ba-loans/src/main/java/com/bankingapi/baloans/controller/LoansController.java
@@ -2,6 +2,7 @@ package com.bankingapi.baloans.controller;
 
 import com.bankingapi.baloans.constants.LoansConstants;
 import com.bankingapi.baloans.dto.ErrorResponseDTO;
+import com.bankingapi.baloans.dto.LoansDTO;
 import com.bankingapi.baloans.dto.ResponseDTO;
 import com.bankingapi.baloans.service.ILoansService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,16 +11,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "CRUD REST APIs for loans in Eazy Bank", description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH and DELETE loan details")
 @RestController
@@ -41,5 +40,30 @@ public class LoansController {
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDTO(LoansConstants.STATUS_201, LoansConstants.MESSAGE_201));
     }
 
-    
+    @Operation(summary = "Fetch Loan Details REST API", description = "REST API to fetch loan details based on a mobile number")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(responseCode = "500", description = "HTTP Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    })
+    @GetMapping("/fetch")
+    public ResponseEntity<LoansDTO> fetchLoanDetails(@RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "MobileNumber must be 10 digits") String mobileNumber) {
+        LoansDTO loansDTO = iLoansService.fetchLoan(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(loansDTO);
+    }
+
+    @Operation(summary = "Update Loan Details REST API", description = "REST API too update loan details based on a loan number")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(responseCode = "417", description = "Expectation Failed"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    })
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDTO> updateLoanDetails(@Valid @RequestBody LoansDTO loansDTO) {
+        boolean isUpdated = iLoansService.updateLoan(loansDTO);
+        if (isUpdated) {
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDTO(LoansConstants.STATUS_200, LoansConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDTO(LoansConstants.STATUS_417, LoansConstants.MESSAGE_417_UPDATE));
+        }
+    }
 }
