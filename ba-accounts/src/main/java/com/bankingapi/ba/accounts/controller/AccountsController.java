d520ea847299d4453bc5bc7c5fbc5e6578caabb4
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-03-08 14:45:29 +0530

    Added AccountsContactInfoDTO

--- ba-accounts/src/main/java/com/bankingapi/ba/accounts/controller/AccountsController.java
+++ ba-accounts/src/main/java/com/bankingapi/ba/accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package com.bankingapi.ba.accounts.controller;
 
 import com.bankingapi.ba.accounts.constants.AccountsConstants;
+import com.bankingapi.ba.accounts.dto.AccountsContactInfoDTO;
 import com.bankingapi.ba.accounts.dto.CustomerDTO;
 import com.bankingapi.ba.accounts.dto.ErrorResponseDTO;
 import com.bankingapi.ba.accounts.dto.ResponseDTO;
@@ -14,6 +15,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,11 +27,23 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "CRUD REST APIs for Accounts in EazyBank")
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
 @Validated
 public class AccountsController {
 
-    private IAccountsService iAccountsService;
+    private final IAccountsService iAccountsService;
+
+    public AccountsController(IAccountsService iAccountsService) {
+        this.iAccountsService = iAccountsService;
+    }
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private AccountsContactInfoDTO accountsContactInfoDTO;
 
     @Operation(summary = "Create Account REST API", description = "REST API to create new Customer")
     @ApiResponses({
@@ -81,5 +97,35 @@ public class AccountsController {
         } else {
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDTO(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE));
         }
+    }
+
+    @Operation(summary = "Get Build information", description = "Get Build information that is deployed into accounts Microservice")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+        @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    })
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo() {
+        return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+    }
+
+    @Operation(summary = "Get Java Version", description = "Get Java Version details that's installed into accounts microservice")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    })
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+    @Operation(summary = "Get Contact Info", description = "Contact Info Details that can be reached out in case of issues")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
+            @ApiResponse(responseCode = "500", description = "HTTP Status Internal Server Error", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class)))
+    })
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountsContactInfoDTO> getContactInfo() {
+        return ResponseEntity.status(HttpStatus.OK).body(accountsContactInfoDTO);
     }
 }
