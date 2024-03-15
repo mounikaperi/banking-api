0989b163a78978588f8fdacf2c17d1c6101ff7cd
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-03-15 18:29:54 +0530

    Initial commit of Eureka Netflix Service Discovery Agent

---
+++ ba-eurekaserver/src/main/java/com/bankingapi/baeurekaserver/BaEurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.bankingapi.baeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaEurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaEurekaserverApplication.class, args);
+	}
+
+}
