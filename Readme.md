e500663e17199a905684e78848d1579af29ff50c
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-03-16 19:12:26 +0530

    Refactoring code

---
+++ Readme.md
@@ -0,0 +1,5 @@
+configserver: http://localhost:8071/
+eurekaserver: http://localhost:8761/eureka/
+ba-accounts: http://localhost:8080/
+ba-cards: http://localhost:8081/
+ba-loans: http://localhost:8082/
--- ba-eurekaserver/src/main/java/com/bankingapi/baeurekaserver/BaEurekaserverApplication.java
+++ ba-eurekaserver/src/main/java/com/bankingapi/baeurekaserver/BaEurekaserverApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class BaEurekaserverApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BaEurekaserverApplication.class, args);
 	}
