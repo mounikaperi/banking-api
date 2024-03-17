7c3247a3fac43681428616b9e47e62f314d57c63
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-03-17 16:48:06 +0530

    Added gateway server

---
+++ ba-gatewayserver/src/main/java/com/bankingapi/bagatewayserver/BaGatewayserverApplication.java
@@ -0,0 +1,13 @@
+package com.bankingapi.bagatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaGatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaGatewayserverApplication.class, args);
+	}
+
+}
---
+++ ba-gatewayserver/src/test/java/com/bankingapi/bagatewayserver/BaGatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingapi.bagatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaGatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
