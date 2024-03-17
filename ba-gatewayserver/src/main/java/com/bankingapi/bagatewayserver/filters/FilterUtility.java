d48e26508b6eda03ac3e9834e84a32d089dbb606
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-03-17 19:21:25 +0530

    Added tracing and logging to api gateway

--- ba-gatewayserver/src/main/java/com/bankingapi/bagatewayserver/filters/FilterUtility.java
+++ ba-gatewayserver/src/main/java/com/bankingapi/bagatewayserver/filters/FilterUtility.java
@@ -1,4 +1,29 @@
 package com.bankingapi.bagatewayserver.filters;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.net.http.HttpHeaders;
+import java.util.List;
+
+@Component
 public class FilterUtility {
+
+    public static final String CORRELATION_ID = "eazybank-correlation-id";
+
+    public String getCorrelationId(HttpHeaders requestHeaders) {
+        List<String> requestHeaderList = requestHeaders.allValues(CORRELATION_ID);
+        if (requestHeaderList.contains(CORRELATION_ID)) {
+            return requestHeaderList.stream().findFirst().get();
+        }
+        return null;
+    }
+
+    public ServerWebExchange setRequestHeader(ServerWebExchange exchange, String name, String value) {
+        return exchange.mutate().request(exchange.getRequest().mutate().header(name, value).build()).build();
+    }
+
+    public ServerWebExchange setCorrelationId(ServerWebExchange exchange, String correlationId) {
+        return this.setRequestHeader(exchange, CORRELATION_ID, correlationId);
+    }
 }
--- ba-gatewayserver/src/main/java/com/bankingapi/bagatewayserver/filters/RequestTraceFilter.java
+++ ba-gatewayserver/src/main/java/com/bankingapi/bagatewayserver/filters/RequestTraceFilter.java
@@ -1,12 +1,16 @@
 package com.bankingapi.bagatewayserver.filters;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
-import java.util.logging.Logger;
+import java.net.http.HttpHeaders;
 
 @Order(1)
 @Component
@@ -15,5 +19,30 @@ public class RequestTraceFilter implements GlobalFilter {
     private static final Logger logger = LoggerFactory.getLogger(RequestTraceFilter.class);
 
     @Autowired
+    FilterUtility filterUtility;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+        if (isCorrelationIdPresent(requestHeaders)) {
+            logger.debug("eazyBank-correlation-id found in RequestTraceFilter: {}", filterUtility.getCorrelationId(requestHeaders));
+        } else {
+            String correlationId = generateCorrelationId();
+            exchange = filterUtility.setCorrelationId(exchange, correlationId);
+            logger.debug("eazyBank-correlation-id generated in RequestTraceFilter: {}", correlationId);
+        }
+        return chain.filter(exchange);
+    }
+
+    private boolean isCorrelationIdPresent(HttpHeaders requestHeaders) {
+        if (filterUtility.getCorrelationId(requestHeaders) != null) {
+            return true;
+        }
+        return false;
+    }
+
+    private String generateCorrelationId() {
+        return java.util.UUID.randomUUID().toString();
+    }
 
 }
