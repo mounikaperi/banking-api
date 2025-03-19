83d27ea45cb2cd040de03659f86988853aa0aaee
mounikaperi <saimounikaperi@gmail.com>
2025-03-19 12:51:36 +0530

    Defining Redis Rate Limiting Pattern

--- accounts/src/main/java/com/bankingapi/accounts/service/client/LoansFallback.java
+++ accounts/src/main/java/com/bankingapi/accounts/service/client/LoansFallback.java
@@ -7,12 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class LoansFallback implements LoansFeignClient{
     @Override
-    public ResponseEntity<LoansDTO> fetchLoanDetails(String correlationId, String mobileNumber) {
+    public ResponseEntity<List<LoansDTO>> fetchLoanDetails(String correlationId, String mobileNumber) {
         LoansDTO loansDTO = new LoansDTO();
         loansDTO.setMessage("Unable to fetch loan details of the customer. Please try again!!!");
-        return ResponseEntity.status(HttpStatus.OK).body(loansDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(List.of(loansDTO));
     }
 }
