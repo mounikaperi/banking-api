67b4532271bf559029f23ef70f8e0151dc9e74be
mounikaperi <saimounikaperi@gmail.com>
2025-03-26 19:08:34 +0530

    Kafka Message microservice

---
+++ message/src/test/java/com/bankingapi/message/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingapi.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
