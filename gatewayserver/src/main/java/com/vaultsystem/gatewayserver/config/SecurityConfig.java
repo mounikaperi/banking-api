695820ccdc2758240a5c9351d359ba66fff7853b
mounikaperi <saimounikaperi@gmail.com>
2025-04-21 11:23:30 +0530

    Defining cors and csrf filters allow only https

--- gatewayserver/src/main/java/com/vaultsystem/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/vaultsystem/gatewayserver/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.vaultsystem.gatewayserver.config;
 
 import com.vaultsystem.gatewayserver.exceptionhandling.CustomAccessDeniedHandler;
 import com.vaultsystem.gatewayserver.exceptionhandling.CustomBasicAuthenticationEntryPoint;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
@@ -10,13 +11,23 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -25,15 +36,35 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.sessionManagement(session -> session.invalidSessionUrl("/invalidSession").maximumSessions(3).maxSessionsPreventsLogin(true))
-                .requiresChannel(channel -> channel.anyRequest().requiresInsecure()) // allow only https
-                .csrf(csrfConfig -> csrfConfig.disable())
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/vaultsystem/register", "/vaultsystem/invalidSession").permitAll()
-                        .requestMatchers("/vaultsystem/*").authenticated()
-                );
-        http.formLogin(withDefaults());
-        http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        http.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                CorsConfiguration config = new CorsConfiguration();
+                                config.setAllowedOrigins(Collections.singletonList("https://localhost:4200"));
+                                config.setAllowedMethods(Collections.singletonList("*"));
+                                config.setAllowCredentials(true);
+                                config.setAllowedHeaders(Collections.singletonList("*"));
+                                config.setExposedHeaders(Arrays.asList("Authorization"));
+                                config.setMaxAge(3600L);
+                                return config;
+                            }
+                        }))
+                        .csrf(csrfConfig -> csrfConfig.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                        .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                                .requiresChannel(rcc -> rcc.anyRequest().requiresSecure())
+                        .authorizeHttpRequests((requests) -> requests
+                            .requestMatchers("/myAccount").hasRole("USER")
+                            .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
+                            .requestMatchers("/myLoans").authenticated()
+                            .requestMatchers("/myCards").hasRole("USER")
+                            .requestMatchers("/user").authenticated()
+                            .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
+        http.oauth2ResourceServer(rsc -> rsc.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
         http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
         return http.build();
     }
